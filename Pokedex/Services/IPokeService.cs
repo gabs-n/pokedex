d6ffe0b070a036e8f2a61caa21e26e45b1471320
gabi <nicoletigabriela2@gmail.com>
2023-03-06 11:36:23 -0300

    Alteração na pasta Home, criação da pasta services e criação de interface

---
+++ Pokedex/Services/IPokeService.cs
@@ -0,0 +1,19 @@
+using Pokedex.Models;
+
+namespace Pokedex.Services;
+public interface IPokeService
+{
+
+    Pokemon GetPokemon(int Numero)
+    {
+
+    }
+    List<Pokemon> GetPokemons()
+    {
+
+    }
+    Lists<Tipo> GetTipos()
+    {
+
+    }
+}
